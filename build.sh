c6a432cccec7779a296308ab9e56090df27eae91
Parth Raghav <iniento12@gmail.com>
2020-09-09 17:54:49 +0530

    :construction_worker: Added build file

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
+SRC_PATH=src/app.js
 BUILD_PATH=src/app.min.js
-MINIFIER_URL=https://javascript-minifier.com/raw
-
-# Adding header
-echo "/* Author: Parth Raghav <raghavp@reed.edu> */" > $BUILD_PATH
-echo -e "// File: app.min.js\n" >> $BUILD_PATH
+TMP_BUILD_PATH=src/_app.min.js
+ERR_LOG_PATH=error_log
+MINIFIER_URL=https://javascript-minifier.com/rw
+BUILD_HEADER="/* Author: Parth Raghav <raghavp@reed.edu> */\n// File: app.min.js\n"
 
 CURL_RETURN_CODE=0
-CURL_OUTPUT=`curl -X POST -s --data-urlencode 'input@src/app.js' $MINIFIER_URL >> $BUILD_PATH` || CURL_RETURN_CODE=$?
-if [ ${CURL_RETURN_CODE} -ne 0 ]; then  
+CURL_OUTPUT=`curl -X POST -s -o $BUILD_PATH --data-urlencode input@$SRC_PATH -w "%{http_code}" $MINIFIER_URL || CURL_RETURN_CODE=$?`
+if [ $CURL_OUTPUT != "200" ]; then  
     echo "🚨Build failed with error code - ${CURL_RETURN_CODE}"
+    # Move error logs
+    cat $BUILD_PATH >> $ERR_LOG_PATH
+    # Clean the build file
+    echo > $BUILD_PATH
 else
     echo "✨Build successful"
+    # Adding header
+    cat <(echo -e "$BUILD_HEADER") $BUILD_PATH > $TMP_BUILD_PATH
+    mv $TMP_BUILD_PATH $BUILD_PATH
 fi
