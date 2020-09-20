b5c54c5f231471e83820b71c394d98713ebc65f7
Ali <alif.taqi@hotmail.com>
2020-09-20 02:37:03 -0700

    .

--- src/app.js
+++ src/app.js
@@ -401,8 +401,8 @@
     rcends: new Date("Feb 1 2020 23:59:59").getTime(), // club registeration deadline
     rfsopens: new Date("Jun 1 2020 23:59:59").getTime(), // senate nomination opens
     rfsends: new Date("Jul 1 2020 23:59:59").getTime(), // senate nomination deadline
-    fpopens: new Date("Oct 11 2020 23:59:59").getTime(), // funding poll begindate
-    fpends: new Date("Oct 12 2020 23:59:59").getTime(), // funding poll closedate, // new Date("Sep 12, 2019 23:59:59").getTime()
+    fpopens: new Date("Sep 11 2020 23:59:59").getTime(), // funding poll begindate
+    fpends: new Date("Sep 21 2020 23:59:59").getTime(), // funding poll closedate, // new Date("Sep 12, 2019 23:59:59").getTime()
     seopens: new Date("July 1 2020 23:59:59").getTime(), // senate election begindate
     seends: new Date("Aug 1 2020 23:59:59").getTime(), // senate election closedate
     campaign_max_length: 250,
